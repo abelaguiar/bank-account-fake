83c092e7f80215ac65d75c3e7ecdf7ff089423d3
Ian Rodrigues <iansrodrigs@gmail.com>
2017-12-05 09:06:56 -0300

    step 2

--- BankAccountTest.php
+++ BankAccountTest.php
@@ -10,4 +10,13 @@ class BankAccountTest extends PHPUnit_Framework_Festcase
 
         $this->assertEquals(0, $balance);
     }
+
+    /** @test */
+    function it_can_initialize_the_balance_via_constructor()
+    {
+        $account = new BankAccount(100);
+        $balance = $account->getBalance();
+
+        $this->assertEquals(100, $balance);
+    }
 }
