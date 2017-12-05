9477c552b66103ff9b716db78dafdf21e77ffd20
Ian Rodrigues <iansrodrigs@gmail.com>
2017-12-05 09:45:15 -0300

    store

---
+++ StoreTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class StoreTest extends TestCase
+{
+    /** @test */
+    function it_has_products()
+    {
+        $store = factory(Store::class)->create();
+        factory(Product::class, 5)->create(['store_id' => $store->id]);
+
+        $this->assertEquals(5, $store->products()->count());
+    }
+}
