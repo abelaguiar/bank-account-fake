1c57c405c02dcff186fa7b0b47e938df32643681
Ian Rodrigues <iansrodrigs@gmail.com>
2017-12-06 09:51:35 -0300

    tests

--- Features/ListProductsByStoreTest.php
+++ Features/ListProductsByStoreTest.php
@@ -6,11 +6,25 @@ class ListProductsByStore extends TestCase
     function list_products_by_store()
     {
         $store = factory(Store::class)->create();
-        factory(Product::class)->create(['store_id' => $store->id]);
+
+        // 5 produtos...
+        factory(Product::class)->create(['store_id' => $store->id, 'name' => 'Arroz']);
+        factory(Product::class)->create(['store_id' => $store->id, 'name' => 'Feijão']);
+        factory(Product::class)->create(['store_id' => $store->id, 'name' => 'Macarrão']);
+        factory(Product::class)->create(['store_id' => $store->id, 'name' => 'Carnes']);
+        factory(Product::class)->create(['store_id' => $store->id, 'name' => 'Peixes']);
 
         $response = $this->get("/api/store/{$store->id}/products");
 
         $response->assertStatus(200);
+        $response->assertJson([
+            ['name' => 'Arroz'],
+            ['name' => 'Feijão'],
+            ['name' => 'Macarrão'],
+            ['name' => 'Carnes'],
+            ['name' => 'Peixes'],
+        ]);
+
         $this->assertCount(5, $response->json());
     }
 }
